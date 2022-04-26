0d565f06180e5b11581d1a3dbbb0d7c24669798b
bagaci05 <blaga.mate05@gmail.com>
2022-04-26 18:54:38 +0200

    sadf

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ function gettime() {
     var h = d.getHours();
     var m = d.getMinutes();
     var s = d.getSeconds();
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (s < 10) {
+        s = "0" + s;
+    }
     
     document.getElementById('ora').innerHTML = h + ":" + m + ":" + s;
     setTimeout(gettime, 1000);
